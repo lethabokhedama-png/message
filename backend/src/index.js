954069089143ccd6d4b4142ee7adeb273925e8a7
lethabokhedama-png <lethabokhedama@gmail.com>
2026-06-28 01:17:58 +0200

    created db.schemas, cors and clerMiddleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,28 @@
 import express from 'express';
+import cors from 'cors';
 import "dotenv/config";
+
+import { clerkMiddleware } from '@clerk/express';
+
+import User from './models/user.model.js';
+import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL
 
-app.listen(PORT, () => console.log("Server is up and running on PORT:", PORT));
+app.use(express.json());
+app.use(cors({
+  origin:FRONTEND_URL,
+  credentials:true 
+}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ healthy : "running"})
+});
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is up and running on PORT:", PORT);
+});
